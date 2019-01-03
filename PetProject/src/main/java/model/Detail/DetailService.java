b50eef8ced5f0d246fe38cd8296ce2e00e4da174
ShenYe <ShenYe@DESKTOP-4JK1UBM>
2019-01-03 18:35:39 +0800

    insert detail service

--- PetProject/src/main/java/model/Detail/DetailService.java
+++ PetProject/src/main/java/model/Detail/DetailService.java
@@ -1,8 +1,41 @@
 package model.Detail;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DetailService {
+	
+	@Autowired
+	private DetailDAO detailDAO;
+	
+	public DetailBean SearchDetail(Integer detailID) {
+		DetailBean bean = detailDAO.findByDetailId(detailID);
+		if(bean != null) {
+			return bean;
+		}
+		return null;
+	}
+	
+	public boolean insertDetail(DetailBean bean) {
+		if(bean != null) {
+			return detailDAO.insert(bean);
+		}
+		return false;
+	}
+	
+	public boolean updateDetail(DetailBean bean) {
+		if(bean.getDetailId() != null) {
+			return detailDAO.update(bean);
+		}
+		return false;
+	}
+	
+	public boolean deleteDetail(DetailBean bean) {
+		if(bean.getDetailId() != null) {
+			return detailDAO.delete(bean);
+		}
+		return false;
+	}
 
 }
