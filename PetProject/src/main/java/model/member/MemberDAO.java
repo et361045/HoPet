82e1cc76b7beb001219da092956a396732cece8e
劉得安 <@xxxx.com>
2019-01-03 16:48:15 +0800

    測試

--- PetProject/src/main/java/model/member/MemberDAO.java
+++ PetProject/src/main/java/model/member/MemberDAO.java
@@ -2,7 +2,7 @@ package model.member;
 
 public interface MemberDAO {
 	public abstract Integer findMemberId(String email);
-	public abstract MemberBean findMemberBean(int MemberId);
+	public abstract MemberBean findMemberBean(int memberId);
 	public abstract MemberBean findMemberBean(String email);
 	public abstract boolean update(MemberBean member);
 	public abstract boolean insert(MemberBean member);
